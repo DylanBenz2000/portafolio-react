b3863ff55632d8b3c19cb76614d9923237588db7
Dylan Ramos <dylanbenz2000@gmail.com>
2024-07-17 16:48:39 -0600

    Se agrego el componente de Presentacion

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,10 +1,12 @@
 import Footer from "../../components/Footer/Footer"
 import Header from "../../components/Header/Header"
+import Presentacion from "../../components/Presentacion/Presentacion"
 
 const Inicio = () => {
     return (
         <>
             <Header />
+            <Presentacion />
             <Footer />
         </>
 
