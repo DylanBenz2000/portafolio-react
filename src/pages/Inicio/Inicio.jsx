372890d26fcfbd2ab620e3ba129eb4f8bc09b0e3
Dylan Ramos <dylanbenz2000@gmail.com>
2024-07-18 17:17:56 -0600

    Se agrego el componente Projects

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,12 +1,14 @@
 import Footer from "../../components/Footer/Footer"
 import Header from "../../components/Header/Header"
 import Presentacion from "../../components/Presentacion/Presentacion"
+import Projects from "../../components/Projects/Projects"
 
 const Inicio = () => {
     return (
         <>
             <Header />
             <Presentacion />
+            <Projects />
             <Footer />
         </>
 
