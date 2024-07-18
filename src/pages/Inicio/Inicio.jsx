fbec00647659025a38e1bd60535da5bf52470978
Dylan Ramos <dylanbenz2000@gmail.com>
2024-07-18 17:23:45 -0600

    Se agrego un container

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -2,14 +2,18 @@ import Footer from "../../components/Footer/Footer"
 import Header from "../../components/Header/Header"
 import Presentacion from "../../components/Presentacion/Presentacion"
 import Projects from "../../components/Projects/Projects"
+import styles from './Inicio.module.css'
 
 const Inicio = () => {
     return (
         <>
+        <div className={styles.container}>
             <Header />
             <Presentacion />
             <Projects />
             <Footer />
+        </div>
+
         </>
 
     )
