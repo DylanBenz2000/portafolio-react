010d43e7c95c64af3375c4b18cabaa80e578e552
Dylan Ramos <dylanbenz2000@gmail.com>
2024-07-22 17:45:11 -0600

    Se mostro la API por medio de la card y fetch

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,31 @@
+import styles from './Card.module.css'
+import arrow from './arrow.png'
+import git from './github.png'
+
+const Card = ({ titulo, descripcion, year, role, imagen, demo, github}) => {
+    return (
+    <>
+        <div className={styles.cuadro}>
+                <img src={imagen} alt="" />
+        </div>
+
+        <div className={styles.about}>
+            <h2>{titulo}</h2>
+            <h3 className={styles.projectAbout}>{descripcion}</h3>
+            <p>PROJECT INFO</p>
+            <p>Year <span>{year}</span></p>
+            <p>Role <span>{role}</span></p>
+
+            <div className={styles.containerAnchors}>
+            <a href={demo} target="_blank" rel="noopener noreferrer" className={styles.anchorDemo}>Live Demo <img src={arrow} alt="" /></a>
+            <a href={github} target="_blank" rel="noopener noreferrer" className={styles.anchorGit}>Ver Github <img src={git} alt="" /></a>
+            </div>
+            
+
+        </div>
+</>
+
+    )
+}
+
+export default Card
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,41 +1,40 @@
 import styles from './Projects.module.css'
-import work from './work.png'
+import Card from '../Card/Card'
+import { useState, useEffect } from 'react'
 
 const Projects = () => {
+
+    const [proyectos, setProyectos] = useState([])
+
+    useEffect( () => {
+        fetch("http://localhost:3000/proyectos")
+        .then(response => response.json())
+        .then(data =>{
+            setProyectos(data)
+        })
+    }, [])
+
     return(
 
-        // TODO ESTO LO HARE DINAMICO DESPUÉS, LO PASARE EN UN JSON o una BD para que después lo itere en un map y pinte la info
+        <>
 
-        <>       
+            <div className={styles.title}>
+                <h1>Proyectos Destacados</h1>
+                <p>Estos son algunos de los proyectos seleccionados que muestran mi pasión por el desarrollo front-end.</p>
+            </div>
 
-                <div className={styles.titlesProjects}>
-                    <h1>Proyectos Destacados</h1>
-                    <p>Estos son algunos de los proyectos seleccionados que muestran mi pasión por el desarrollo front-end.</p>
-                </div>
-
-            <section className={styles.containerProjects}>
-                
-                <div className={styles.projects}>
-                    {/* Aca ira una etiqueta después la pondre que diga que tipo de trabajo es
-                    si es un challenge o proyecto propio, etc.
-                    */}
-                    <img src={work} alt="" />
-
-                </div>
-
-                <div className={styles.about}>
-                    <h2>Promotional landing page for our favorite show</h2>
-                    <h3 className={styles.projectAbout}>Teamed up with a designer to breathe life into a promotional webpage for our beloved show, Adventure Time. Delivered a fully responsive design with dynamic content capabilities, seamlessly integrating a newsletter feature to keep fans updated with the latest adventures.</h3>
-                    <p>PROJECT INFO</p>
-                    <p>Year <span>2023</span></p>
-                    <p>Role <span>Front-End Developer</span></p>
-                    <a href="">Live Demo</a>
-                    <a href="">Ver Github</a>
-                </div>
-
-
+            <section className={styles.container}>
+                {
+                    proyectos.map( (proyecto) => {
+                        return <Card  key={proyecto.nombre} {...proyecto}/>
+                    })
+                }
             </section>
+        
         </>
+
+        
+        
 
 
     )
