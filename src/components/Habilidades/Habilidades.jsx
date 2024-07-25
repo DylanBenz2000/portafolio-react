abc8035c72f2209a2bd653a8beddbfa62ea3c994
Dylan Ramos <dylanbenz2000@gmail.com>
2024-07-25 17:55:25 -0600

    Se agrego el color a CajaHabilidades

---
+++ src/components/Habilidades/Habilidades.jsx
@@ -0,0 +1,23 @@
+import styles from './Habilidades.module.css'
+import CajaHabilidades from '../CajaHabilidades/CajaHabilidades'
+
+const Habilidades = () => {
+    return(
+        <section className={styles.containerHabilidades}>
+            <h1>Habilidades</h1>
+            <div className={styles.habilidades}>
+                <p>Siempre estoy buscando adquirir nuevas habilidades e ir actualizando las tecnologías o librerías que uso. Estas son habilidades que actualmente tengo conocimiento.</p>
+                <div className={styles.cajaHab}>
+                    <CajaHabilidades color="#E34F26">HTML <i className="fa-brands fa-html5"></i></CajaHabilidades>
+                    <CajaHabilidades color="#1572B6">CSS <i className="fa-brands fa-css3-alt"></i></CajaHabilidades>
+                    <CajaHabilidades color="#F7DF1E">JavaScript <i className="fa-brands fa-js"></i></CajaHabilidades>
+                    <CajaHabilidades color="#61DAFB">React <i className="fa-brands fa-react"></i></CajaHabilidades>
+                    <CajaHabilidades color="#DD0031">Angular <i className="fa-brands fa-angular"></i></CajaHabilidades>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+
+export default Habilidades
