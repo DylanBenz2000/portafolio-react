ea50421872408075c159dafb767437dc6b74d78c
Dylan Ramos <dylanbenz2000@gmail.com>
2024-07-25 17:53:09 -0600

    Se removio el componente Header y Footer

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,17 +1,21 @@
+import AboutMe from "../../components/AboutMe/AboutMe"
 import Footer from "../../components/Footer/Footer"
 import Header from "../../components/Header/Header"
 import Presentacion from "../../components/Presentacion/Presentacion"
 import Projects from "../../components/Projects/Projects"
 import styles from './Inicio.module.css'
+import Contacto from "../../components/Contacto/Contacto"
 
 const Inicio = () => {
     return (
         <>
         <div className={styles.container}>
-            <Header />
+            {/* <Header /> */}
             <Presentacion />
             <Projects />
-            <Footer />
+            <AboutMe />
+            <Contacto />
+            {/* <Footer /> */}
         </div>
 
         </>
