d75ebb7d1d4e9b6f122732e5c91d424b8f7c38bd
Dylan Ramos <dylanbenz2000@gmail.com>
2024-07-25 17:54:02 -0600

    Se agrego el componente Header y Footer al componente Routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,24 @@
 import { Routes, BrowserRouter, Route } from "react-router-dom"
 import Inicio from "./pages/Inicio/Inicio"
+import MensajeEnviado from "./pages/MensajeEnviado/MensajeEnviado"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
 
 const AppRoutes = () =>{
     return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Inicio />} />
-            </Routes>
-        </BrowserRouter>
+
+        <div style={{margin: '0 20px'}}>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Inicio />} />
+                    <Route path="/mensaje-enviado" element={<MensajeEnviado />} />
+                </Routes>
+
+                <Footer />
+            </BrowserRouter>
+        </div>
+
     )
 }
 
