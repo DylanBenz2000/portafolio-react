f9560a275fa0a6e9eeee6fcdfe70eaf0714d3d7c
Dylan Ramos <dylanbenz2000@gmail.com>
2024-07-25 17:47:48 -0600

    Se cambio la fecha

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className={styles.footer}>
             <div className={styles.container}>
-                <p className={styles.footerTitle}>© Dylan Ramos 2023</p>
+                <p className={styles.footerTitle}>© Dylan Ramos 2024</p>
             </div>
         </footer>
     )
