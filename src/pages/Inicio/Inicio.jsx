2af4fb39ff31f977255e31389f2fa9f5a4053d5e
Dylan Ramos <dylanbenz2000@gmail.com>
2024-07-27 23:22:15 -0600

    Cambios nuevos en el responsive

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,6 +1,5 @@
 import AboutMe from "../../components/AboutMe/AboutMe"
-import Footer from "../../components/Footer/Footer"
-import Header from "../../components/Header/Header"
+
 import Presentacion from "../../components/Presentacion/Presentacion"
 import Projects from "../../components/Projects/Projects"
 import styles from './Inicio.module.css'
@@ -9,14 +8,14 @@ import Contacto from "../../components/Contacto/Contacto"
 const Inicio = () => {
     return (
         <>
-        <div className={styles.container}>
+
             {/* <Header /> */}
             <Presentacion />
             <Projects />
             <AboutMe />
             <Contacto />
             {/* <Footer /> */}
-        </div>
+
 
         </>
 
